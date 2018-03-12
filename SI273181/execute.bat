d2b707c6f73e8a981dfec284693c7029938985b6
santhoshraddp <E1>
2018-03-12 15:54:32 +0530

    ommit

---
+++ SI273181/execute.bat
@@ -0,0 +1,16 @@
+cd C:\Users\SA389141\Desktop\BasicExpressnode\downloads\SI273181
+ git clone https://santhoshraddp:Santhosh123@github.com/santhoshraddp/First.git
+git
+git init
+git config --global user.name "santhoshraddp"
+git config --global user.email "E1"
+git config --global credential.helper wincred
+git add SI273181
+git commit -m "  ommit"
+git remote -v
+git remote set-url origin https://santhoshraddp:Santhosh123@github.com/santhoshraddp/First.git
+git remote add origin https://github.com/santhoshraddp/First.git
+git push -u origin   master
+echo Press Enter...
+pause
+
